d6f9164c54875fbda06a4dc06da418e5ddbaf87b
mdalamin1964 <mdalamincoder@gmail.com>
2023-11-05 11:50:34 +0600

    upload 3

--- main.jsx
+++ main.jsx
@@ -5,10 +5,8 @@ import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-    <React.StrictMode>
-      <HashRouter>
+      <BrowserRouter basename={`/${process.env.PUBLIC_URL}`}>
           <App />
-      </HashRouter>
-    </React.StrictMode>
+      </BrowserRouter>
   </>
 )
