277b8dfed7f10307e772abc92128b27c1ac01d1f
mdalamin1964 <mdalamincoder@gmail.com>
2023-11-05 14:09:52 +0600

    upload 5

--- src/sidebars/ModelsBox.jsx
+++ src/sidebars/ModelsBox.jsx
@@ -5,28 +5,28 @@ export default function ModelsBox() {
             <div className="models-box">
                 <ul>
                     <li>
-                        <img className='models-box-img' src="./src/assets/taycan@2x.webp" alt="" />
+                        <img className='models-box-img' src="https://raw.githubusercontent.com/mdAlamin1964/porsche/main/src/assets/taycan@2x.webp" alt="" />
                     </li>
                     <li>
-                        <img className='models-box-img' src="./src/assets/taycan@2x.webp" alt="" />
+                        <img className='models-box-img' src="https://raw.githubusercontent.com/mdAlamin1964/porsche/main/src/assets/taycan@2x.webp" alt="" />
                     </li>
                     <li>
-                        <img className='models-box-img' src="./src/assets/taycan@2x.webp" alt="" />
+                        <img className='models-box-img' src="https://raw.githubusercontent.com/mdAlamin1964/porsche/main/src/assets/911@2x.webp" alt="" />
                     </li>
                     <li>
-                        <img className='models-box-img' src="./src/assets/taycan@2x.webp" alt="" />
+                        <img className='models-box-img' src="https://raw.githubusercontent.com/mdAlamin1964/porsche/main/src/assets/macan@2x.webp" alt="" />
                     </li>
                     <li>
-                        <img className='models-box-img' src="./src/assets/taycan@2x.webp" alt="" />
+                        <img className='models-box-img' src="https://raw.githubusercontent.com/mdAlamin1964/porsche/main/src/assets/taycan@2x.webp" alt="" />
                     </li>
                     <li>
-                        <img className='models-box-img' src="./src/assets/taycan@2x.webp" alt="" />
+                        <img className='models-box-img' src="https://raw.githubusercontent.com/mdAlamin1964/porsche/main/src/assets/macan@2x.webp" alt="" />
                     </li>
                     <li>
-                        <img className='models-box-img' src="./src/assets/taycan@2x.webp" alt="" />
+                        <img className='models-box-img' src="https://raw.githubusercontent.com/mdAlamin1964/porsche/main/src/assets/taycan@2x.webp" alt="" />
                     </li>
                     <li>
-                        <img className='models-box-img' src="./src/assets/taycan@2x.webp" alt="" />
+                        <img className='models-box-img' src="https://raw.githubusercontent.com/mdAlamin1964/porsche/main/src/assets/cayenne@2x.webp" alt="" />
                     </li>
                 </ul>
             </div>
